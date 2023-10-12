fa8e3c438dc449f2ecdfe84832f2d1389969d7dd
Cyoungju <dudwn3528@gmail.com>
2023-10-12 23:30:54 +0900

    commit
    commit

--- ShoppingMall/src/main/java/Main.java
+++ ShoppingMall/src/main/java/Main.java
@@ -1,8 +1,20 @@
-package org.example;
+//package org.example;
+
+import Controller.MainController;
 
 // Shift을(를) 두 번 눌러 전체 검색 대화상자를 열고 'show whitespaces'를 입력한 다음,
 // Enter를 누르세요. 그러면 코드 내에서 공백 문자를 확인할 수 있습니다.
 public class Main {
     public static void main(String[] args) {
+        MainController mainController = new MainController();
+        //scene.initialize();
+
+        mainController.setScene(0);
+        while (true){
+            mainController.render();
+            mainController.update();
+
+        }
+
     }
 }
