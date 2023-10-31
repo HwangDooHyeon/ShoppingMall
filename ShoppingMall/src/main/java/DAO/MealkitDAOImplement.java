238548d125e83862d619abcc878d3f4d860a023f
jieun <oljy0515@naver.com>
2023-10-31 21:57:34 +0900

    test

--- ShoppingMall/src/main/java/DAO/MealkitDAOImplement.java
+++ ShoppingMall/src/main/java/DAO/MealkitDAOImplement.java
@@ -11,7 +11,7 @@ import java.sql.SQLException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
-
+//import java.util.*;
 public class MealkitDAOImplement implements MealkitDAO {
 
     private static MealkitDAOImplement instance = new MealkitDAOImplement();
